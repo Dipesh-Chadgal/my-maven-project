bdbb59cdef32cd34599fc685c01cd72a9a0f70d5
Dipesh-Chadgal <dipeshchadgal10@gmail.com>
2025-03-11 13:32:42 +0530

    first commit

---
+++ src/main/java/com/Main.java
@@ -0,0 +1,28 @@
+package com;
+
+import com.model.Author;
+import com.model.Book;
+
+public class Main {
+	
+	
+	
+	public static void main(String[] args) {
+		Book book = new Book();
+		book.setTitle("Harry Java");
+		Author author = new Author();
+		author.setName("JK Schildt");
+		book.setAuthor(author);
+		
+		System.out.println(book);
+		
+		
+		Book book1 = new Book();
+		book1.setTitle("baba bazuka");
+		Author author1 = new Author();
+		author1.setName("Fsociety");
+		book1.setAuthor(author1);
+		
+		System.out.println(book1);
+	}
+}
---
+++ src/main/java/com/model/Book.java
@@ -0,0 +1,29 @@
+package com.model;
+
+public class Book {
+
+	private String title;
+	private Author author;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author.getName();
+	}
+
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", author=" + author + "]";
+	}
+
+}
